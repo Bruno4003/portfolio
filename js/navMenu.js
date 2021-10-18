deb065610c4694d527d1e24d4f84f14dbfa37ff4
Bruno Florencio <brunofmartimiano00@gmail.com>
2021-10-18 19:43:06 -0300

    Add files via upload

---
+++ js/navMenu.js
@@ -0,0 +1,7 @@
+function hamburguerMenu() {
+  document.getElementById("navMenu").style.width = "100%";
+}
+
+function closeHamburguer() {
+  document.getElementById("navMenu").style.width = "0%";
+}
